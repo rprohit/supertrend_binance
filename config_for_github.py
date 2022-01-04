7fdb4a497e7a9807c7c16f9ab19a13765e474b8f
Rohit <Welcome1>
2022-01-04 17:04:12 +0530

    Added dummy config file for github

---
+++ config_for_github.py
@@ -0,0 +1,209 @@
+API_KEY = 'Your api key '
+API_SECRET = 'Your Secret'
+
+
+
+#Money Management Config
+TOTAL_USDT = 1370
+NO_OF_COINS_OF_INTEREST = 20
+PARK_RATIO = 0 #Parking money for averaging in percentage
+
+
+#Global Variables
+per_per_trade = 5
+time_frame = '5m'
+total_balance_for_supertrend = 1000
+ema_period = 200
+
+trades = [
+    {
+        'coin_symbol': 'CRV/USDT'
+    },
+    {
+        'coin_symbol': 'GALA/USDT'
+    },
+    {
+        'coin_symbol': 'LUNA/USDT'
+    },
+    {
+        'coin_symbol': 'POWR/USDT'
+    },
+    {
+        'coin_symbol': 'DOT/USDT'
+    },
+    {
+        'coin_symbol' : 'BNB/USDT'
+    },
+    {
+        'coin_symbol' : 'SOL/USDT'
+    },
+    {
+        'coin_symbol' : 'AVAX/USDT'
+    },
+    {
+        'coin_symbol' : 'ADA/USDT'
+    },
+    {
+        'coin_symbol' : 'XRP/USDT'
+    },
+    {
+        'coin_symbol' : 'LUNA/USDT'
+    },
+    {
+        'coin_symbol' : 'DOGE/USDT'
+    },
+    {
+        'coin_symbol' : 'SHIB/USDT'
+    },
+    {
+        'coin_symbol' : 'MATIC/USDT'
+    },
+    {
+        'coin_symbol' : 'ALGO/USDT'
+    },
+    {
+        'coin_symbol' : 'LTC/USDT'
+    },
+    {
+        'coin_symbol' : 'UST/USDT'
+    },
+    {
+        'coin_symbol' : 'LINK/USDT'
+    },
+    {
+        'coin_symbol' : 'NEAR/USDT'
+    },
+    {
+        'coin_symbol' : 'BCH/USDT'
+    },
+    {
+        'coin_symbol' : 'TRX/USDT'
+    },
+    {
+        'coin_symbol' : 'ATOM/USDT'
+    },
+    {
+        'coin_symbol' : 'XLM/USDT'
+    },
+    {
+        'coin_symbol' : 'FTM/USDT'
+    },
+    {
+        'coin_symbol' : 'MANA/USDT'
+    },
+    {
+        'coin_symbol' : 'AXS/USDT'
+    },
+    {
+        'coin_symbol' : 'HBAR/USDT'
+    },
+    {
+        'coin_symbol' : 'VET/USDT'
+    },
+    {
+        'coin_symbol' : 'FTT/USDT'
+    },
+    {
+        'coin_symbol' : 'SAND/USDT'
+    },
+    {
+        'coin_symbol' : 'ICP/USDT'
+    },
+    {
+        'coin_symbol' : 'FIL/USDT'
+    },
+    {
+        'coin_symbol' : 'THETA/USDT'
+    },
+    {
+        'coin_symbol' : 'AVAX/USDT'
+    },
+    {
+        'coin_symbol' : 'ONE/USDT'
+    },
+    {
+        'coin_symbol' : 'GXS/USDT'
+    },
+    {
+        'coin_symbol' : 'YFI/USDT'
+    },
+    {
+        'coin_symbol' : 'ELF/USDT'
+    },
+    {
+        'coin_symbol' : 'FLUX/USDT'
+    },
+    {
+        'coin_symbol' : 'ROSE/USDT'
+    },
+    {
+        'coin_symbol' : 'SUSHI/USDT'
+    },
+    {
+        'coin_symbol' : 'SXP/USDT'
+    },
+    {
+        'coin_symbol' : 'ENJ/USDT'
+    },
+    {
+        'coin_symbol' : 'VOXEL/USDT'
+    },
+    {
+        'coin_symbol' : 'ARDR/USDT'
+    },
+    {
+        'coin_symbol' : 'AAVE/USDT'
+    },
+    {
+        'coin_symbol' : 'ALGO/USDT'
+    },
+    {
+        'coin_symbol' : 'CHR/USDT'
+    },
+    {
+        'coin_symbol' : 'SYS/USDT'
+    },
+    {
+        'coin_symbol' : 'ANT/USDT'
+    },
+    {
+        'coin_symbol' : 'ANKR/USDT'
+    },
+    {
+        'coin_symbol' : 'CHR/USDT'
+    },
+    {
+        'coin_symbol' : 'SYS/USDT'
+    },
+    {
+        'coin_symbol' : 'EOS/USDT'
+    },
+    {
+        'coin_symbol' : 'CHZ/USDT'
+    },
+    {
+        'coin_symbol' : 'JST/USDT'
+    },
+    {
+        'coin_symbol' : 'CHESS/USDT'
+    },
+    {
+        'coin_symbol' : 'AXS/USDT'
+    },
+    {
+        'coin_symbol' : 'KAVA/USDT'
+    },
+    {
+        'coin_symbol' : 'PEOPLE/USDT'
+    },
+    {
+        'coin_symbol' : 'QTUM/USDT'
+    },
+    {
+        'coin_symbol' : 'UTK/USDT'
+    },
+    {
+        'coin_symbol' : 'LTC/USDT'
+    }
+
+]
